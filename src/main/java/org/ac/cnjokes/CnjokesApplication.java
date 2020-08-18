2ff9955688d1ec9f9ca39a3f8bc611bc90758f9a
Eldurlin <adam.cakala@gmail.com>
2020-08-18 14:32:42 +0200

    initial commit

---
+++ src/main/java/org/ac/cnjokes/CnjokesApplication.java
@@ -0,0 +1,13 @@
+package org.ac.cnjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnjokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnjokesApplication.class, args);
+	}
+
+}
